50c8956ede9f601518665803611a5469478de344
Jaime Isidro <jaime@deudigital.com>
2018-01-18 17:34:48 -0400

    cors

--- nutri/app/Http/Middleware/Cors.php
+++ nutri/app/Http/Middleware/Cors.php
@@ -13,7 +13,13 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+	 public function handle($request, Closure $next)
+     {
+		return $next($request)
+			->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+			->header('Access-Control-Allow-Headers', 'content-type, withcredentials, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+      }
+    public function handle__old($request, Closure $next)
     {
         return $next($request)
 			->header('Access-Control-Allow-Origin', '*')
--- nutri/public/index.php
+++ nutri/public/index.php
@@ -20,16 +20,13 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
-/*$allowedOrigins = array(
-    '*',
-    'http://localhost',
+$allowedOrigins = array(
     'http://localhost:4200',
-    'http://127.0.0.1:8000',
 );
 
 if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
     foreach ($allowedOrigins as $allowedOrigin) {
-        if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+        if (@preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
             header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
             header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
             header('Access-Control-Max-Age: 1000');
@@ -37,7 +34,7 @@ if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
             break;
         }
     }
-}*/
+}
 require __DIR__.'/../vendor/autoload.php';
 
 /*
