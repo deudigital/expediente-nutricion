c60e617f8917d76e65f3a44fa7e88b1064eb34fc
Jesus Soto <sotous0@gmail.com>
2018-02-13 18:46:55 -0400

    bug with error message Factura-Vacia fixed

--- ngnutritrack/src/app/factura-vacia/factura-vacia.component.ts
+++ ngnutritrack/src/app/factura-vacia/factura-vacia.component.ts
@@ -629,18 +629,20 @@ export class FacturaVaciaComponent implements OnInit {
 
 	facturar(){
 
+		if(this.queryPerson || this.queryPerson.length===0){
+			this.form_errors.empty_name= true;
+		}
+
 		if(this.productos.length===0){
 			this.form_errors.empty_products = true;
 		}
 
-		if(this.persona.queryPerson && this.persona.queryPerson===""){
-			this.form_errors.empty_name= true;
-		}
+		
 
 		/*if((this.persona.cedula && this.persona.nombre) && (this.persona.telefono && this.persona.email) 
 			&& (!this.form_errors.invalid_id && !this.form_errors.invalid_phone) && !this.form_errors.invalid_email){	*/
 
-		if(this.persona.nombre && !this.form_errors.empty_products && !this.form_errors.empty_name){
+		if(this.persona.nombre || !this.form_errors.empty_products || !this.form_errors.empty_name){
 
 			this.loading = true;
 			if(!this.seleccionado){	
