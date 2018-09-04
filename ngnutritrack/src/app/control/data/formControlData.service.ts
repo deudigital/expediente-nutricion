2bba078040e79cfc657a59e6068d96b990d59a2a
Jaime Isidro <jaime@deudigital.com>
2018-09-04 06:15:11 -0400

    fixing..

--- ngnutritrack/src/app/control/data/formControlData.service.ts
+++ ngnutritrack/src/app/control/data/formControlData.service.ts
@@ -24,7 +24,7 @@ import {Observable} from 'rxjs/Rx';
 export class FormControlDataService {
 	private formControlData: FormControlData = new FormControlData();
 	private apiURL	=	environment.apiUrl;
-	private _apiURL	=	environment.apiUrlsec;
+	private _apiURL	=	'http://dev.expediente.nutricion.co.cr/nutri/public/api/v1/';
 	private headers: Headers;
 	data:any={};
 
