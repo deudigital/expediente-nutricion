ccb150c4b74ae345e3586b741ce0b842b75a911a
Jaime Isidro <jaime@deudigital.com>
2018-11-12 22:37:29 -0400

    email templates

---
+++ nutri/resources/views/emails/test.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <!--[if !mso]><!-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!--<![endif]-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title></title>
+  <style type="text/css">
+.ReadMsgBody { width: 100%; background-color: #ffffff; }
+.ExternalClass { width: 100%; background-color: #ffffff; }
+.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div { line-height: 100%; }
+html { width: 100%; }
+body { -webkit-text-size-adjust: none; -ms-text-size-adjust: none; margin: 0; padding: 0; }
+table { border-spacing: 0; table-layout: fixed; margin: 0 auto; }
+table table table { table-layout: auto; }
+.yshortcuts a { border-bottom: none !important; }
+img:hover { opacity: 0.9 !important; }
+a { color: #cc1f25; text-decoration: none; }
+.textbutton a { font-family: 'open sans', arial, sans-serif !important; }
+.btn-link a { color: #FFFFFF !important; }
+
+@media only screen and (max-width: 480px) {
+body { width: auto !important; }
+*[class="table-inner"] { width: 90% !important; text-align: center !important; }
+*[class="table-full"] { width: 100% !important; text-align: center !important; }
+/* image */
+img[class="img1"] { width: 100% !important; height: auto !important; }
+}
+</style>
+</head>
+
+<body>
+  <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+      <td align="center">
+        <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0">
+          <tr>
+            <td align="center" bgcolor="#414a51" background="https://expediente.nutricion.co.cr/bg-login@2x.6c503c7d8db2c72eaf47.jpg" style="background-size:cover; background-repeat:repeat-x; background-position:top;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td width="500" align="center">
+                    <table class="table-inner" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                      <tr>
+                        <td height="50"></td>
+                      </tr>
+                     
+                      <tr>
+                        <td height="20"></td>
+                      </tr>
+                      <tr>
+                        <td align="center">
+                          <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                              <td width="35" align="right" valign="bottom">
+                                <table width="35" border="0" align="right" cellpadding="0" cellspacing="0">
+                                  <tr>
+                                    <td height="25"></td>
+                                  </tr>
+                                  <tr>
+                                    <td height="25" bgcolor="#FFFFFF" style="border-top-left-radius:6px; font-size:0px;">&nbsp;</td>
+                                  </tr>
+                                </table>
+                              </td>
+                              <td align="center" background="https://expediente.nutricion.co.cr/mail/images/title-bg.png" style="background-image: url(https://expediente.nutricion.co.cr/mail/images/title-bg.png); background-repeat: repeat-x; background-size: auto; background-position: bottom;">
+                                <table style="border-radius:6px;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#90c445">
+                                  <tr>
+                                    <td height="50" align="center">
+                                      <table border="0" align="center" cellpadding="0" cellspacing="0" class="table-inner">
+                                        <tr>
+                                          <td height="7"></td>
+                                        </tr>
+                                        <tr>
+                                          <td align="center" style="font-family: 'Open sans', Arial, sans-serif; color:#FFFFFF; font-size:16px; font-weight: bold;">Cita cancelada</td>
+                                        </tr>
+                                        <tr>
+                                          <td height="7"></td>
+                                        </tr>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </table>
+                              </td>
+                              <td width="35" align="left" valign="bottom">
+                                <table width="35" border="0" align="left" cellpadding="0" cellspacing="0">
+                                  <tr>
+                                    <td height="25"></td>
+                                  </tr>
+                                  <tr>
+                                    <td height="25" bgcolor="#FFFFFF" style="border-top-right-radius:6px; font-size:0px;">&nbsp;</td>
+                                  </tr>
+                                </table>
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                    </table>
+                    <table class="table-inner" bgcolor="#FFFFFF" width="100%" border="0" cellspacing="0" cellpadding="0">
+                      <tr>
+                        <td height="30"></td>
+                      </tr>
+                      <!--logo-->
+                      <tr>
+                        <td align="center">
+                          <a href="#">
+                            <img style="display:block; line-height:0px; font-size:0px; border:0px; width: 150px" src="https://expediente.nutricion.co.cr/assets/images/logo.png" alt="logo" />
+                          </a>
+                        </td>
+                      </tr>
+                      <!--end logo-->
+                      <tr>
+                        <td height="25"></td>
+                      </tr>
+                      <tr>
+                        <td bgcolor="#f3f3f3" align="center">
+                          <table align="center" class="table-inner" width="90%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                              <td height="40"></td>
+                            </tr>
+                            
+                            <tr>
+                              <td height="25"></td>
+                            </tr>
+                            <!--content-->
+                            <tr>
+                              <td align="center" style="font-family: 'Open sans', Arial, sans-serif; color:#7f8c8d; font-size:14px; line-height: 28px;">
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet tellus massa. Fusce sit amet massa ut lorem molestie blandit. Quisque ut leo id neque pellentesque eleifend nec eget odio. Sed id velit non leo dictum lacinia. Sed dictum tellus sed elit maximus, porttitor facilisis tortor pulvinar. Duis imperdiet semper arcu, a interdum metus gravida vel. Vivamus ut mi ultricies ipsum hendrerit cursus. Curabitur sed tincidunt erat. Cras ultricies mauris aliquam mauris ultrices, vitae ornare ligula tristique. In varius lectus et massa posuere, quis gravida metus sodales. Praesent volutpat urna a blandit semper.</p>
+
+									<p>Nullam vulputate quam et luctus lacinia. Nam varius ipsum eget pharetra ullamcorper. Aliquam pretium lacinia pellentesque. Nulla eu faucibus eros. Curabitur ut libero quis erat consectetur laoreet. Sed at vulputate erat. Cras rhoncus erat eu enim mattis, luctus cursus libero maximus. Donec ultricies justo quis quam malesuada posuere vitae a velit.</p>
+
+									<p>Morbi elit ipsum, accumsan non pellentesque sed, semper ut ante. Etiam eget magna finibus, congue diam eu, scelerisque justo. Pellentesque suscipit leo nunc, eu blandit orci dapibus id. Integer justo quam, porttitor sed dictum quis, dictum gravida mauris. Integer posuere tincidunt arcu tempus consectetur. Cras eu condimentum est. Nam blandit mauris est, eu facilisis lacus aliquet vitae.</p>
+
+									<p>Vivamus tristique porta dolor placerat imperdiet. Cras in urna sapien. Quisque a metus mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer sit amet fringilla augue. Ut nibh nibh, pellentesque a magna a, egestas luctus elit. Maecenas eu varius arcu. Duis pellentesque ipsum at maximus laoreet. Etiam vestibulum turpis eget velit egestas, et euismod lorem mattis. Maecenas in urna velit. Integer condimentum augue et suscipit sodales. Duis volutpat tellus eget augue tempor, ac aliquet velit scelerisque. Cras id neque eget justo rutrum facilisis. Maecenas neque nibh, mattis id dolor at, condimentum congue ligula. Vestibulum est urna, aliquet at pulvinar sed, bibendum non mi. Proin varius elit vitae enim egestas, et egestas massa fermentum.</p>
+                              </td>
+                            </tr>
+                            <!--end content-->
+                            <tr>
+                              <td height="40"></td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td height="35"></td>
+                      </tr>
+                      <tr>
+                        <td height="45" align="center" style=" box-shadow:0px 3px 0px #ccd5dc;border-bottom-left-radius:6px;border-bottom-right-radius:6px;"></td>
+                      </tr>
+                    </table>
+                   
+                        </td>
+                      </tr>
+                      <tr>
+                        <td height="45"></td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
