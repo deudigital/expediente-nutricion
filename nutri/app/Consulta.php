e86fdd84ba220ddd7b08495f680df102f1012daf
Jaime Isidro <jaime@deudigital.com>
2019-09-12 01:25:12 -0400

    Adicionado el campo para notas para el paciente, y carga de archivos para nutricionista y paciente

--- nutri/app/Consulta.php
+++ nutri/app/Consulta.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Consulta extends Model
 {
     protected $fillable = [
-        'fecha',  'notas', 'estado', 'paciente_id'
+        'fecha',  'notas',  'notas_paciente', 'estado', 'paciente_id'
     ];
 	public $timestamps = false;
 }
--- nutri/routes/api.php
+++ nutri/routes/api.php
@@ -42,6 +42,7 @@ Route::group(['middleware' => ['cors'], 'prefix' => 'v0'], function(){
 	Route::get('agenda/servicios/{nutricionista_id}', 'AgendaController@selectServicios');
 	Route::get('consultas/{id}/resumen/{mode}', 'ConsultaController@testResumen');
 	Route::get('consultas/patronmenu/duplicados', 'PatronMenuController@duplicados');
+	Route::get('consultas/patronmenu/testing', 'PatronMenuController@FixUpdateDietasVariacionCalorica');
 	Route::get('dietas/paciente/{id}', 'DietaController@belongsToPaciente');
 	Route::get('patronmenu/paciente/{id}', 'DietaController@patronmenuBelongsToPaciente');
 	
@@ -49,7 +50,8 @@ Route::group(['middleware' => ['cors'], 'prefix' => 'v0'], function(){
 	Route::get('prescripcion/paciente/{id}', 'PrescripcionController@belongsToPaciente');
 	Route::get('consultas/nutricionista/{id}/pendientes/', 'ConsultaController@pendientes');
 });
-Route::group(['middleware' => ['auth:api', 'cors'], 'prefix' => 'v1'], function(){
+/*Route::group(['middleware' => ['auth:api', 'cors'], 'prefix' => 'v1'], function(){*/
+Route::group(['middleware' => ['cors'], 'prefix' => 'v1'], function(){
 	Route::get('form/data', 'FormController@dataform');
 	Route::get('graphics/{method}/{indicator}/{id}', 'GraphicController@getIndicators');
 	
@@ -149,6 +151,8 @@ Route::group(['middleware' => ['auth:api', 'cors'], 'prefix' => 'v1'], function(
 	Route::post('consultas/otrosmultiple', 'OtrosAlimentoController@storemultiple');
 	Route::post('consultas/{id}/delete', 'ConsultaController@destroy');
 	Route::post('otrosalimentos/{id}/delete', 'OtrosAlimentoController@destroy');
+
+	Route::post('consultas/archivos', 'ConsultaController@archivos');
 
 	Route::get('pacientes/{id}/patologiashcp', 'PacienteController@patologiashcp');
 	Route::post('pacientes/hcpotros', 'PacienteController@hcpOtros');
