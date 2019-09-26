615d5670a88b4531dad3b01cd650effdef1cb750
Jaime Isidro <jaime@deudigital.com>
2019-09-25 23:53:40 -0400

    archivo environment para staging, y cambio de url de apiurl

--- ngnutritrack/src/environments/environment.prod.ts
+++ ngnutritrack/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   baseUrl:	'https://expediente.nutricion.co.cr',
   loginUrl: 'https://expediente.nutricion.co.cr/nutri/public/api/web/',
-  apiUrl:	'https://expediente.nutricion.co.cr/nutri/public/api/v0/',
+  apiUrl:	'https://expediente.nutricion.co.cr/nutri/public/api/v1/',
   apiUrlsec:'https://expediente.nutricion.co.cr/nutri/public/api/v1/',
   
   
